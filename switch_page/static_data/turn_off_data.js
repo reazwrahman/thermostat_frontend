ece095c50a311656dd7344a0d8a0d984e11c0289
Reaz Rahman <reazrahman@Reazs-Laptop.local>
2024-06-11 19:51:22 -0400

    backing up

--- switch_page/static_data/turn_off_data.js
+++ switch_page/static_data/turn_off_data.js
@@ -1,10 +1,7 @@
 const static_turn_off_data={
-    "message": "Device needs to be on for at least another 0.97 minutes",
+    "message": "Device needs to be on for at least another 0.16 minutes",
     "minimum_on_time": "1 minutes",
-    "status": "REQUEST_DENIED",
-    "time_elapsed": "0.03 minutes",
-    "time_remaining": "0.97 minutes",
-    "timestamp": "2024-06-09 19:51"
+    "status": "REQUEST_DENIED"
 }; 
 
 export {static_turn_off_data};
--- switch_page/static_data/turn_on_data.js
+++ switch_page/static_data/turn_on_data.js
@@ -1,10 +1,6 @@
 const static_turn_on_data={
-    "cool_down_period": "1 minutes",
-    "message": "Device needs to be in cool down for another 0.97 minutes",
-    "status": "REQUEST_DENIED",
-    "time_elapsed": "0.03 minutes",
-    "time_remaining": "0.97 minutes",
-    "timestamp": "2024-06-09 16:02"
+    "status": "TURNED_ON",
+    "timestamp": "2024-06-11 19:51"
 }; 
 
 export {static_turn_on_data};
