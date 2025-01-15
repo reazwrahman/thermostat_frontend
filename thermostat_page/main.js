e6c429044a4afd90d3a9573d44cb23984b523858
Reaz Rahman <reazrahman@tempo.fios-router.home>
2025-01-15 01:06:43 -0500

    round fixed

--- thermostat_page/main.js
+++ thermostat_page/main.js
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Populate select with values from 10 to 35 in increments of 0.5
     for (let i = 15; i <= 30; i += 0.2) {
         const optOn = document.createElement('option');
-        optOn.value = i;
-        optOn.textContent = i;
+        optOn.value = Number(i.toFixed(2));
+        optOn.textContent = Number(i.toFixed(2));
         temperatureSelectOn.appendChild(optOn);  
 
         const optUpdate = document.createElement('option');
